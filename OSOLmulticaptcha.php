57dc54d8c1e6e9955834f0276dad54da18614e4d
Hariyom <hariyom.com@gmail.com>
2021-04-26 12:44:29 +0530

    Fixed deprecated array reference in curly braces bug
    Added css tweak to fix bug of dissappearing captcha image. Thanks to the contribution by Haris Stupar :)

--- OSOLmulticaptcha.php
+++ OSOLmulticaptcha.php
@@ -95,7 +95,7 @@ class OSOLmulticaptcha{
 		while(true){
 				$this->keystring='';
 				for($i=0;$i<$this->captchaLength;$i++){
-					$this->keystring.=$allowed_symbols{mt_rand(0,strlen($allowed_symbols)-1)};
+					$this->keystring.=$allowed_symbols[mt_rand(0,strlen($allowed_symbols)-1)];
 				}
 				if(!preg_match('/cp|cb|ck|c6|c9|rn|rm|mm|co|do|cl|db|qp|qb|dp|ww/', $this->keystring)) break;
 			}
@@ -295,13 +295,13 @@ class OSOLmulticaptcha{
 				$transparent = (imagecolorat($font, $i, 0) >> 24) == 127;
 
 				if(!$reading_symbol && !$transparent){
-					$font_metrics[$alphabet{$symbol}]=array('start'=>$i);
+					$font_metrics[$alphabet[$symbol]]=array('start'=>$i);
 					$reading_symbol=true;
 					continue;
 				}
 
 				if($reading_symbol && $transparent){
-					$font_metrics[$alphabet{$symbol}]['end']=$i;
+					$font_metrics[$alphabet[$symbol]]['end']=$i;
 					$reading_symbol=false;
 					$symbol++;
 					continue;
@@ -319,7 +319,7 @@ class OSOLmulticaptcha{
 			// draw text
 			$x=1;
 			for($i=0;$i<$length;$i++){
-				$m=$font_metrics[$this->keystring{$i}];
+				$m=$font_metrics[$this->keystring[$i]];
 
 				$y=mt_rand(-$fluctuation_amplitude, $fluctuation_amplitude)+($height-$fontfile_height)/2+2;
 
